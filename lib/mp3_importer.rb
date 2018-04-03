11fb4434656d417c15fbc90e27082d909d2a0ad3
Greg Stimson <stimsong@yahoo.com>
2018-04-03 18:11:51 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -21,9 +21,11 @@ class MP3Importer
   end
 
   def import
-    Song.new_by_filename(@alt_filenames)
+    alt_filenames.each do |file|
+      Song.new_by_filename(file)
+    end
   end
-  
+
 
 
 end
